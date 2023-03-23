12d41e6c66c08f5afa9e83f4d516967d9934d34c
Hassan-Memon <hassan.ghaddai@live.com>
2023-03-23 10:32:10 +0500

    [Feature] Incorporate a SQL database and implement CRUD operations

--- planner/main.py
+++ planner/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from routes.users import user_router
 from routes.events import event_router
+from database.connection import conn
 
 import uvicorn
 
@@ -8,5 +10,17 @@ app = FastAPI()
 
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
+
+
+@app.on_event("startup")
+def on_startup():
+    conn()
+
+
+# @app.get('/')
+# async def home():
+#     return RedirectResponse(url="/events/")
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
