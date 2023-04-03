a7d6cc2fc233abbf07128f2b6a70a45ea90f6e51
Hassan-Memon <hassan.ghaddai@live.com>
2023-04-03 15:34:33 +0500

    docker deploument git status

--- planner/main.py
+++ planner/main.py
@@ -9,4 +9,4 @@ app = FastAPI()
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
